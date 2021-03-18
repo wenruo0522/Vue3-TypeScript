67bbd4328206d6dbdcdbc4b2fbbb6ece77031c98
wenruo0522 <zjm023@mail.ustc.edu.cn>
2021-03-19 01:05:37 +0800

    add：添加axios并配置简单拦截器

--- vue3-basic/src/main.ts
+++ vue3-basic/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import router from './router'
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+axios.interceptors.request.use(config => {
+    config.params = { ...config.params, icode: 'EDA487AFE20C48FC' }
+    return config
+})
+
+axios.get('columns', { params: { key: 'test' } }).then(res => {
+    console.log('res:', res)
+})
 
 const app = createApp(App)
 
